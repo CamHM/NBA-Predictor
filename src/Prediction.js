8fa1a18159fb495e31fb544b16a6ad8036b4e6b0
CamHM <cami.andre469@gmail.com>
2020-09-23 23:48:17 -0500

    :tada: Hey ho! let's go"

--- src/Prediction.js
+++ src/Prediction.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { BackButton } from "./Team";
 import logos from "./lgos.json";
+import MaterialIcon from "material-icons-react";
 
 const Prediction = () => {
     const [predictions, setPredictions] = useState([]);
+    const [result, setResult] = useState([]);
 
     const style = {
         page: {
@@ -30,14 +32,22 @@ const Prediction = () => {
             marginLeft: '40px',
             fontSize: '30px',
         },
+        statsGroup: {
+            backgroundColor: 'rgba(255, 255, 255, 0.05)',
+            margin: '10px',
+            borderRadius: '5px',
+        }
     };
 
     useEffect(() => {
         fetch('https://ic-nba.herokuapp.com/prediction_result')
             .then(res => res.json())
-            .then(data => setPredictions(data.games))
+            .then(data => {
+                setPredictions(data.games);
+                setResult(data.season_result);
+            })
     }, []);
-    console.log(predictions)
+
     return (
         <div style={style.page}>
             <BackButton />
@@ -49,6 +59,9 @@ const Prediction = () => {
             </div>
             <div style={style.stats}>
                 <h1 style={style.sectionTitle}>Resultados</h1>
+                <div style={style.statsGroup}>
+                    { result.sort((a, b) => (a.total_win > b.total_win) ? -1 : 1).map((r, i) => <PositionCard key={`position-${i}`} position={r} />) }
+                </div>
             </div>
         </div>
     )
@@ -79,6 +92,9 @@ const MatchCard = ({ match }) => {
         title: {
             color: '#61dafb',
             marginBottom: '20px',
+        },
+        winner: {
+            marginTop: '10px',
         }
     };
 
@@ -89,16 +105,56 @@ const MatchCard = ({ match }) => {
                 <div>
                     <p style={style.label}>Local</p>
                     <img src={logos[match.home_team]} alt={match.home_team} style={style.logo} />
+                    <p style={style.label}>P: {match.prediction_home_team}%</p>
                 </div>
-                <h1>VS</h1>
+                <h1 style={style.label}>VS</h1>
                 <div>
                     <p style={style.label}>Visitante</p>
                     <img src={logos[match.away_team]} alt={match.away_team} style={style.logo} />
+                    <p style={style.label}>P: {match.prediction_away_team}%</p>
                 </div>
             </div>
-            <div>
+            <div style={style.winner}>
                 <p style={style.label}>Ganador</p>
                 <img src={logos[match.winner]} alt={match.winner} style={style.logo} />
+            </div>
+            <p style={style.label}>{match.date}</p>
+        </div>
+    )
+};
+
+const PositionCard = ({ position }) => {
+    const style = {
+        card: {
+            margin: '10px 5px',
+            padding: '30px 30px 0 30px',
+            display: 'flex',
+
+        },
+        logo: {
+            width: '60px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        text: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: '20px',
+        }
+    };
+
+    return (
+        <div style={style.card}>
+            <img src={logos[position.team]} alt={position.team} style={style.logo} />
+            <div className="wins" style={style.text}>
+                <MaterialIcon icon="arrow_upward" color="#5EBC00" />
+                <h3>Ganados: {position.total_win}</h3>
+            </div>
+            <div className="loss" style={style.text}>
+                <MaterialIcon icon="arrow_downward" color="#FF3D13" />
+                <p>Perdidos: {position.total_loss}</p>
             </div>
         </div>
     )
